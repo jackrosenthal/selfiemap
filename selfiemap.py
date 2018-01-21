3809d55deca1f48dae65d07a33fd68462cec6204
Jack Rosenthal <jack@rosenth.al>
2018-01-21 08:35:17 +0100

    Loading screen

--- selfiemap.py
+++ selfiemap.py
@@ -11,6 +11,7 @@ images_url = 'https://inmotion.adrivo.com/images/300/uploads/user/fcb/{}_preview
 
 class Window(threading.Thread):
     def __init__(self):
+        self.done_loading = False
         self.texture = sf.Texture.from_file("map.png")
         self.world = sf.Sprite(self.texture)
         self.video_mode = sf.VideoMode.get_fullscreen_modes()[0]
@@ -19,6 +20,12 @@ class Window(threading.Thread):
         self.world.position = (c / 2 for c in vm_size)
         self.world.ratio = (min(v / t for t, v in zip(self.texture.size, vm_size)) ,) * 2
         self.original_ratio = self.world.ratio.x
+        self.fc_logo = sf.Sprite(sf.Texture.from_file("fcbayern.png"))
+        self.fc_logo.origin = 200, 200
+        self.fc_logo.position = (c / 2 for c in vm_size)
+        self.fade = sf.RectangleShape(vm_size)
+        self.fade.fill_color = sf.Color(0, 0, 0, 127)
+        self.fade.position = (0, 0)
         self.window = sf.RenderWindow(self.video_mode, "FanMap")
         self.window.framerate_limit = 60
         self.q = Queue()
@@ -54,21 +61,26 @@ class Window(threading.Thread):
                     self.world.ratio = (x * 2 for x in self.world.ratio)
             self.window.clear()
             self.window.draw(self.world)
-            while True:
-                try:
-                    lati, longi, obj = self.q.get_nowait()
-                    obj.position = self.lcoord_to_win(
-                            f(c) for f, c in zip((self.lonpx, self.latpx),
-                                                (longi, lati))
-                    )
-                    self.objects.append((obj, 0))
-                except Empty:
-                    break
-            for obj, clock in self.objects:
-                scale = sin(clock/200 * pi)
-                obj.ratio = scale, scale
-                self.window.draw(obj)
-            self.objects = [(o, c + 1) for o, c in self.objects if c < 200]
+            if not self.done_loading:
+                self.window.draw(self.fade)
+                self.fc_logo.rotation = (self.fc_logo.rotation + 1) % 360
+                self.window.draw(self.fc_logo)
+            else:
+                while True:
+                    try:
+                        lati, longi, obj = self.q.get_nowait()
+                        obj.position = self.lcoord_to_win(
+                                f(c) for f, c in zip((self.lonpx, self.latpx),
+                                                    (longi, lati))
+                        )
+                        self.objects.append((obj, 0))
+                    except Empty:
+                        break
+                for obj, clock in self.objects:
+                    scale = sin(clock/200 * pi)
+                    obj.ratio = scale, scale
+                    self.window.draw(obj)
+                self.objects = [(o, c + 1) for o, c in self.objects if c < 200]
             self.window.display()
 
     def latpx(self, lat):
@@ -124,17 +136,26 @@ class SelfiesDownloader(threading.Thread):
                 continue
             self.q.put(tuple(selfie[:-1]) + (r.content, ))
 
+class DataLoader(threading.Thread):
+    def run(self):
+        global selfie_data
+        global cities
+        cities = defaultdict(dict)
+        with open('data/images.csv') as f:
+            selfie_data = [l.split(',') for l in f.read().splitlines()]
+        with open('data/cities.csv') as f:
+            for line in f:
+                code, unaccent, accent, _, _, lati, longi = line.split(',')
+                for name in unaccent, accent:
+                    cities[code.casefold()][name.casefold()] = tuple(map(float, (lati, longi)))
+        w.done_loading = True
+
 if __name__ == '__main__':
     w = Window()
     w.start()
-    with open('data/images.csv') as f:
-        selfie_data = [l.split(',') for l in f.read().splitlines()]
-    cities = defaultdict(dict)
-    with open('data/cities.csv') as f:
-        for line in f:
-            code, unaccent, accent, _, _, lati, longi = line.split(',')
-            for name in unaccent, accent:
-                cities[code.casefold()][name.casefold()] = tuple(map(float, (lati, longi)))
+    loader = DataLoader()
+    loader.start()
+    loader.join()
     g = TestDataGenerator(w)
     g.daemon = True
     g.start()
